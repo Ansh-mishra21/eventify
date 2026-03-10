036ee268cadf09a8a76fec029419e94f98794d35
Ansh-mishra21 <mishransh2108@gmail.com>
2026-03-10 17:58:46 +0530

    Refactor AuthContext and axios instance for improved readability and consistency

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,15 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
+// Create axios instance with backend base URL
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: "http://localhost:5000/api",
 });
 
+// Interceptor to automatically attach JWT token to every request
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
